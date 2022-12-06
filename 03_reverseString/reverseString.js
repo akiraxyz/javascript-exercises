f4939a4e86a2de730f7353104954199325ff77b6
akiraxyz <akiraxyz@protonmail.com>
2022-12-06 16:32:48 +0100

    Exercises Finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+	return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+reverseString("Hello");
+reverseString("Hello World");
+reverseString("Hello, Wold 3.1415");
+reverseString("");
