c29a3a042db6bbbf4cd7d8c0626458208756ef3e
akiraxyz <akiraxyz@protonmail.com>
2022-12-06 16:24:38 +0100

    Exercises Finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+	if (times < 0) return "ERROR";
+	let string = "";
+	for (let i = 0; i < times; i++) {
+		string += word;
+	}
+	return string;
 };
 
-// Do not edit below this line
 module.exports = repeatString;
+
+repeatString("hey", 3);
+repeatString("hey", 10);
+repeatString("hey", 1);
+repeatString("hey", 0);
+repeatString("hey", -1);
+repeatString("hey", Math.floor(Math.random() * 20));
+repeatString("", 10);
